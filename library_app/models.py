d49a58ebbe9f10adfda2051c230ec4d12a08ab6a
toriium <jorge.r.p.v@hotmail.com>
2026-06-21 18:24:17 +0200

    feat: add author

--- library_app/models.py
+++ library_app/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 
-class Book(models.Model):
+class Author(BaseModel):
+    name = models.CharField(max_length=150)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        managed = True
+
+
+class Book(BaseModel):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, related_name="books")
     publication_year = models.IntegerField()
 
     def __str__(self):
@@ -13,4 +30,4 @@ class Book(models.Model):
 
     class Meta:
         managed = True
-        # db_table = 'book'
+

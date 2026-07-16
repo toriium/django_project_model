7d9fe5d7949260ebeddeaee6e34dcbea846ed884
toriium <jorge.r.p.v@hotmail.com>
2026-07-16 16:12:04 +0200

    a

---
+++ library_app/views/__init__.py
@@ -0,0 +1,5 @@
+from .home import home
+from .books_table import books_table
+from .people_table import people_table
+from .trigger import trigger_404, trigger_500
+from .public import public
--- library_app/views/public.py
+++ library_app/views/public.py
@@ -1,7 +1,5 @@
-
 from django.contrib.auth.decorators import login_not_required
 from django.shortcuts import render
-
 
 
 @login_not_required
